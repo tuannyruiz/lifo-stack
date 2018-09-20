417e35a7182b3390640b6c8633d092b6ec905c17
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-09-19 21:13:56 -0300

    create algorithm to convert decimal number in binary

--- Stack.js
+++ Stack.js
@@ -46,3 +46,23 @@ let Stack = (function() {
 
 	return Stack;
 })();
+
+// Decimal para binário
+
+function divideBy2 (decNumber) {
+	let remStack = new Stack(),
+			rem,
+			binaryString = '';
+
+	while (decNumber > 0) {
+		rem = Math.floor(decNumber % 2);
+		remStack.push(rem);
+		decNumber = Math.floor(decNumber / 2);
+	}
+
+	while (!remStack.isEmpty()) {
+		binaryString += remStack.pop().toString();
+	}
+
+	return binaryString;
+}
